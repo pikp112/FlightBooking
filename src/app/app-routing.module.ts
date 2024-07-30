1f83d5765b4a3cfa7e22483ac6f273337bac5628
Sorin Giurgica <H3XM@it7.eurofins.com>
2024-07-30 15:28:48 +0300

    added components and routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './pages/website/search/search.component';
+import { BookFlightComponent } from './pages/website/book-flight/book-flight.component';
+import { MyBookingsComponent } from './pages/website/my-bookings/my-bookings.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
+import { AirportComponent } from './pages/admin/airport/airport.component';
+import { FlightsComponent } from './pages/admin/flights/flights.component';
+import { LoginComponent } from './pages/admin/login/login.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: 'search', component: SearchComponent, title: 'Search Flights' },
+  { path: 'book-flight', component: BookFlightComponent, title: 'Book Flight' },
+  { path: 'bookings', component: MyBookingsComponent, title: 'My Bookings' },
+  { path: 'login', component: LoginComponent, title: 'Login' },
+  { path: '', component: LayoutComponent, children: [
+    { path: 'airport', component: AirportComponent},
+    { path: 'flights', component: FlightsComponent}
+  ]}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/admin/login/login.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
+import { AirportComponent } from './pages/admin/airport/airport.component';
+import { FlightsComponent } from './pages/admin/flights/flights.component';
+import { BookingComponent } from './pages/admin/booking/booking.component';
+import { CityComponent } from './pages/admin/city/city.component';
+import { NewFlightComponent } from './pages/admin/new-flight/new-flight.component';
+import { SearchComponent } from './pages/website/search/search.component';
+import { BookFlightComponent } from './pages/website/book-flight/book-flight.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MyBookingsComponent } from './pages/website/my-bookings/my-bookings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LayoutComponent,
+    AirportComponent,
+    FlightsComponent,
+    BookingComponent,
+    CityComponent,
+    NewFlightComponent,
+    SearchComponent,
+    BookFlightComponent,
+    MyBookingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
